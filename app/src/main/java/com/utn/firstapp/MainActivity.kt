b9cd71f2bfdfb5fa0691bb6bfb7e9f0a7db34921
Ale Sobral 147.886-2 <asobral@frba.utn.edu.ar>
2023-03-28 22:32:51 -0300

    First push

---
+++ app/src/main/java/com/utn/firstapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.utn.firstapp
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var label: TextView
+    private lateinit var btnShow: Button
+    private lateinit var btnHide: Button
+    private lateinit var btnRed: Button
+    private lateinit var btnBlue: Button
+
+    var labelText: String = "Hi Text "
+    var labelTextVoid: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        label = findViewById(R.id.MyFirstTextView)
+        btnHide = findViewById(R.id.btnHide)
+        btnShow = findViewById(R.id.btnShow)
+        btnRed = findViewById(R.id.btnRed)
+        btnBlue = findViewById(R.id.btnBlue)
+
+
+        btnShow.setOnClickListener {
+            label.text = labelText
+        }
+
+        btnHide.setOnClickListener {
+            label.text = labelTextVoid
+        }
+
+        btnRed.setOnClickListener {
+            label.setTextColor(Color.parseColor("#0aad3f"))
+        }
+
+        btnBlue.setOnClickListener {
+            label.setTextColor(Color.parseColor("#000d3f"))
+        }
+
+    }
+}
